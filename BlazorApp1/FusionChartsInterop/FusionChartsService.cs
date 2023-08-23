116e9e28ef824ff3ddd6a1d9f2c041a8839e9c9b
5380harshjain <harsh.jain1@accolitedigital.com>
2023-08-23 13:11:51 +0530

    In progress

--- BlazorApp1/FusionChartsInterop/FusionChartsService.cs
+++ BlazorApp1/FusionChartsInterop/FusionChartsService.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.RenderTree;
 using Microsoft.JSInterop;
-​
 namespace Microsoft.FusionChartsInterop
 {
     public class FusionChartsService
@@ -10,16 +9,12 @@ namespace Microsoft.FusionChartsInterop
         {
             _jsruntime = jSRuntime;
         }
-        
         // Rendering Fusion chart//
-​
         public async Task renderChart(String chartConfig)
         {
             await _jsruntime.InvokeVoidAsync("FusionCharts.renderChart", chartConfig);
         }
-​
         // To activate the valid license//
-​
         public async Task activateLicense(String licenseKey)
         {
             var licenseObject = new
@@ -29,14 +24,11 @@ namespace Microsoft.FusionChartsInterop
             };
             await _jsruntime.InvokeVoidAsync("FusionCharts.options.license", licenseObject);
         }
-​
         //Genric Method calling another method generic method written in blazor-fusionCharts.js which is further calling fusion charts method//
-​
         public async Task<String> CallFusionChartsFunction(String functionName, String chartId, params object[] args)
         {
            String result = await _jsruntime.InvokeAsync<String>("FusionCharts.invokeChartFunction", functionName, chartId, args);
            return result;
         }
-​
     }
 }
--- BlazorApp1/wwwroot/blazor-fusionCharts.js
+++ BlazorApp1/wwwroot/blazor-fusionCharts.js
@@ -2,6 +2,7 @@ function parseFunction(key, value) {
         if (typeof (value) === 'string' && value.includes('function()')) {
             let regEx = new RegExp("{([^}]*)}");
             let func = new Function(value.split("function()")[1]);
+            console.log(value.split("function()")[1]);
             return func;
         }
         return value
@@ -15,6 +16,28 @@ function parseFunction(key, value) {
     //Generic Method to call any fusion chart method exluding except above methods//
     window.FusionCharts.invokeChartFunction = (functionName, chartID, ...args) => {
         var currentChart = FusionCharts(chartID);
+
+        if(functionName === "addEventListener"){
+            console.log(args[0][1]);
+            let value = args[0][1];
+            let funcString = value.split("function() {")[1];
+            let func = new Function(funcString.substring(0, funcString.length - 1));
+            //let func = new Function(value);
+            //let func = new Function(value.split("function()")[1]);
+            console.log(func, "this is function");
+            console.log(typeof func);
+
+            //console.log(eval(value), "using eval here");
+
+            //var result = currentChart[functionName].apply(currentChart, func);
+            var result = currentChart[functionName][func];
+
+            console.log(result);
+
+            return "Undefined";
+
+        }
+
         var result =   currentChart[functionName].apply(currentChart, ...args);
         var typeofresult = typeof result;
         if(typeofresult === "number" || typeofresult === "boolean"){
