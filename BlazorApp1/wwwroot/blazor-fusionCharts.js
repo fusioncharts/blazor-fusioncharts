d597071ee8632e3244d107e78204c58b62a9a508
Meher Howji <meher.howji@idera.com>
2023-08-30 11:20:30 +0530

    minor update

--- BlazorApp1/wwwroot/blazor-fusionCharts.js
+++ BlazorApp1/wwwroot/blazor-fusionCharts.js
@@ -1,44 +1,52 @@
-const FUNCTION_KEYWORD = 'function()';
+const FUNCTION_KEYWORD = "function()";
+const CALLBACK = "callback";
 
-// reviver function, key is passed by the JSON.parse
+// Reviver function, key is passed by the JSON.parse
 function parseFunction(key, value) {
   if (typeof value === "string" && value.includes(FUNCTION_KEYWORD)) {
-    let functionParts = value.split("function()")
-    let functionBody = functionParts[1]
+    let functionParts = value.split("function()");
+    let functionBody = functionParts[1];
     let parsedFn = new Function(functionBody);
     return parsedFn;
   }
-  // if the value is not function then return as is
+  // if the value is not function then return it as-is
   return value;
 }
-// Responsible for rendering any chart//
+
+// Responsible for rendering any chart
 window.FusionCharts.renderChart = (chartConfiguration) => {
   const configAsJSObject = JSON.parse(chartConfiguration, this.parseFunction);
   const chart = new FusionCharts(configAsJSObject);
   chart.render();
 };
-//Generic Method to call any fusion chart method exluding except above methods//
+
+//Generic Method to call any fusion chart method exluding except above methods
 window.FusionCharts.invokeChartFunction = (functionName, chartID, ...args) => {
+  // data is an array
+  let currentChart = FusionCharts(chartID),
+    result;
 
-  var currentChart = FusionCharts(chartID);
+  console.log(chartID, currentChart, functionName, args, " <<< invokeChartFunction log");
 
-  if (functionName === 'addEventListener' || (args.length > 0 && args[0][0] === "callback")) {
-
-    let event = args[0][1]
-    let functionAsString = args[0][2];
-    let callbackFn = parseFunction(null, functionAsString);
-    var result = currentChart[functionName].call(currentChart, event, callbackFn);
-
+  if (args[0].type === CALLBACK) {
+    let { event, fn } = args[0];
+    let callbackFn = parseFunction(null, fn);
+    result = currentChart[functionName].call(currentChart, event, callbackFn);
+    return String(result);
+  } else if (args[0].returnType === "NULL") {
+    // for functions that return the chart instance object the return type needs to be defined as the
+    // object contains circular deps which throws exception in blazor code while serialising and deserialising
+    currentChart[functionName].apply(currentChart, userData);
+  } else {
+    result = currentChart[functionName].apply(currentChart, ...args);
+    if (typeof result === "object") {
+      // this might fail if object return has circular deps
+      // if the user needs this data then it needs to be processes in the JS file only, 
+			// it cannot be sent to the razor file
+      result = JSON.stringify(result);
+    } else if (typeof result === "XML") {
+      result = XML.stringify(result);
+    }
     return String(result);
   }
-
-  var result = currentChart[functionName].apply(currentChart, ...args);
-  var typeofresult = typeof result;
-  if (typeofresult === "object") {
-    result = JSON.stringify(result);
-  } else if (typeofresult === "XML") {
-    result = XML.stringify(result);
-  }
-  
-  return String(result);
 };
