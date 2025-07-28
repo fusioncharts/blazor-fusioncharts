4ee81e035b01efff285eb6c7e67436fd336d778b
AnkitSharma <AnkitSharma@DESKTOP-7FP6410>
2025-07-28 19:09:11 +0530

    - Added spaces before the comments. - Updated the inline comments. - Updated the code to handle the if-else check.
    - The 'Issue':
    The existing implementation of the setDataStore method updated the chart's dataSource with a new dataSource object which only has the data property, ignoring all other properties assigned during initialization. As a result, the chart lost its original dataSource configuration and retained only the data property provided within the setDataStore method.
    
    - 'WHY' this fix works:
    We now retrieve the existing dataSource using the getJSONData method and update its data property. This approach ensures that the updated dataSource maintains its previously assigned properties while reflecting the new value in the data property.

--- BlazorFusionCharts/wwwroot/blazor-fusionCharts.js
+++ BlazorFusionCharts/wwwroot/blazor-fusionCharts.js
@@ -68,25 +68,22 @@ window.FusionCharts.renderChart = (chartConfiguration) => {
 
 // Method written for charts like time-series chart
 window.FusionCharts.setDataStore = (id, args) => {
-  let currentChart = FusionCharts(id);
-  let fusionDataStore = new FusionCharts.DataStore();
+    let currentChart = FusionCharts(id);
+    let fusionDataStore = new FusionCharts.DataStore();
 
-  let data = JSON.parse(args[0]);
-  let schema = JSON.parse(args[1]);
+    let data = JSON.parse(args[0]);
+    let schema = JSON.parse(args[1]);
 
-  let fusionTable = fusionDataStore.createDataTable(data, schema);
+    let fusionTable = fusionDataStore.createDataTable(data, schema);
 
-  //Get the existing dataSource attached to the chart
-  let currentDataSource = currentChart.getJSONData();
-  if (currentDataSource !== undefined && currentDataSource !== null) {
-      //Update the "data" property only of the existing dataSource.
-      currentDataSource.data = fusionTable;
-      currentChart.setChartData(currentDataSource);
-  }
-  else {
-      //setChartData is considering below as a new dataSource with "data" property only, other dataSource elements are missing in below.
-      currentChart.setChartData({ data: fusionTable });
-  }
+    // Get the existing dataSource attached to the chart
+    let currentDataSource = currentChart.getJSONData() || {};
+
+    // Assign the fusionTable to the data property of the dataSource
+    currentDataSource.data = fusionTable;
+
+    // Set the updated dataSource back to the chart
+    currentChart.setChartData(currentDataSource);
 };
 
 // resizeTo will return circular json object which cannot be stringified
